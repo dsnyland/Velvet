a651be24f2404bdf087811c73892ee040d34cd54
Sayatoshi <sayatoshi@protonmail.com>
2025-09-14 22:41:22 +0100

    ♻️ refactor(utils): extract utilities into separate `@velvet/utils` package

---
+++ packages/utils/src/logging/logs.ts
@@ -0,0 +1,57 @@
+import { getCallerInfo } from "../misc/lib";
+import { LogType } from "./types";
+
+const BASE_PREFIX = "&r&4Velvet&r 🞙 ";
+
+const logColour = {
+  info: "&9",
+  log: "&7",
+  warn: "&e",
+  error: "&4",
+};
+
+export const log = (type: LogType, data: unknown) => {
+  const timestamp = new Date().toISOString();
+  const caller = getCallerInfo();
+
+  const prefix = `${BASE_PREFIX}&7${timestamp}&r ${logColour[type]}[${type.toUpperCase()}]&r &8${caller} -&r`;
+
+  console?.[type ?? "log"]?.(`${colourise(prefix)}`, data);
+};
+
+export const consoleColours: Record<string, string> = {
+  "&r": "\x1b[0m", // reset
+  "&l": "\x1b[1m", // bold/bright
+  "&o": "\x1b[3m", // italic
+  "&n": "\x1b[4m", // underline
+  "&m": "\x1b[9m", // strike-through
+  "&k": "\x1b[8m", // obfuscated
+
+  "&0": "\x1b[30m", // black
+  "&1": "\x1b[34m", // dark blue
+  "&2": "\x1b[32m", // dark green
+  "&3": "\x1b[36m", // dark aqua / cyan
+  "&4": "\x1b[31m", // dark red
+  "&5": "\x1b[35m", // dark purple / magenta
+  "&6": "\x1b[33m", // gold / yellow
+  "&7": "\x1b[37m", // gray
+  "&8": "\x1b[90m", // dark gray
+  "&9": "\x1b[94m", // blue
+
+  "&a": "\x1b[92m", // green
+  "&b": "\x1b[96m", // aqua / light cyan
+  "&c": "\x1b[91m", // red
+  "&d": "\x1b[95m", // light purple
+  "&e": "\x1b[93m", // yellow
+  "&f": "\x1b[97m", // white
+};
+
+export const colourise = (text: string): string => {
+  for (const [code, colour] of Object.entries(consoleColours)) {
+    text = text.replace(
+      new RegExp(code.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"), "g"),
+      colour,
+    );
+  }
+  return text;
+};
