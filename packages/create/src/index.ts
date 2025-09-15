9ceab7ea29b5be6c19b259026122cad4d24347c4
Sayatoshi <sayatoshi@protonmail.com>
2025-09-15 21:27:27 +0100

    🎨 feat(cli): improve path handling, logging visuals, and CLI UX

--- packages/create/src/index.ts
+++ packages/create/src/index.ts
@@ -1,11 +1,57 @@
 #!/usr/bin/env node
 
+import { colourise, gradientise } from "@velvet/utils";
 import { ask } from "./cli/input/lib";
 import { QuestionType } from "./cli/input/types";
+import { clearTerminalLines } from "./cli/utils/terminal";
+
+const emptyPadding = "          ";
+const prefixPadding = "  ";
+
+const normalisePath = (path: string): string => {
+  let normalisedPath = path.replace(/\/+$/, "");
+
+  if (!/^(?:\.\/|\.\.\/|\/)/.test(normalisedPath)) {
+    normalisedPath = `./${normalisedPath}`;
+  }
+
+  return normalisedPath;
+};
+
+const prefix = (title: string): string =>
+  gradientise(` ${title} `, "#e4a055", "#ce4a77", {
+    background: true,
+    foregroundHex: "#000000",
+  });
 
 (async () => {
-  const reply = await ask("1. Write a reply: ", QuestionType.TEXT);
-  console.log("Reply is:", reply, "\n");
+  let home = await ask(
+    prefix("▸ home") +
+    prefixPadding +
+    "Let's pick a home for your project!\n" +
+    emptyPadding,
+    QuestionType.TEXT,
+  );
+
+  clearTerminalLines(1);
+
+  home = normalisePath(home as string);
+  console.log(
+    `${emptyPadding}${gradientise(`${home}`, "#6e6e6e", "#6e6e6e")}\n`,
+  );
+
+  const name = await ask(
+    prefix("▸ name") +
+    prefixPadding +
+    "Now let's pick a name for your project!\n" +
+    emptyPadding,
+    QuestionType.TEXT,
+  );
+
+  clearTerminalLines(1);
+  console.log(
+    `${emptyPadding}${gradientise(`${name}`, "#6e6e6e", "#6e6e6e")}\n`,
+  );
 
   const option = await ask("2. Pick an option:", QuestionType.RADIO, [
     "Option 1",
